398a05ec5cfd38416525510b974d9af50e96a6a6
Rick Cid <rick@hispand.com>
2015-06-02 09:37:04 -0600

    Added containers and styling to todo list

--- app/List.js
+++ app/List.js
@@ -4,36 +4,36 @@ var List  = React.createClass({
   render: function(){
 
     var styles = {
-    uList: {
-      paddingLeft: 0,
-      listStyleType: "none"
-    },
-    listGroup: {
-      margin: '5px 0',
-      borderRadius: 5
-    },
-    removeItem: {
-      fontSize: 20,
-      float: "left",
-      position: "absolute",
-      top: 12,
-      left: 6,
-      cursor: "pointer",
-      color: "rgb(222, 79, 79)"
-    },
-    todoItem: {
-      paddingLeft: 20,
-      fontSize: 17
-    }
+      uList: {
+        paddingLeft: 0,
+        listStyleType: "none"
+      },
+      listGroup: {
+        margin: '5px 0',
+        borderRadius: 5
+      },
+      removeItem: {
+        fontSize: 20,
+        float: "left",
+        position: "absolute",
+        top: 12,
+        left: 6,
+        cursor: "pointer",
+        color: "rgb(222, 79, 79)"
+      },
+      todoItem: {
+        paddingLeft: 20,
+        fontSize: 17
+      }
     };
     var listItems = this.props.items.map(function(item, index){
       return (
         <li key={index} className="list-group-item" style={styles.listGroup}>
-          <span
+          <button
             className="glyphicon glyphicon-remove"
-            style={styles.removeItem}
+            style={styles.removeItem} 
             onClick={this.props.remove.bind(null, index)}>
-          </span>
+          </button>
           <span style={styles.todoItem}>
             {item}
           </span>
